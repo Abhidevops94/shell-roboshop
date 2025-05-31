c822475febe564b48fc9ce819ad35f6758827f34
Abhi <abhidevops94@gmail.com>
2025-05-31 22:37:16 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -1,37 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+source ./common.sh
+app_name=mongodb
 
-
-mkdir -p $LOGS_FOLDER
-echo "script started excuting at: $(date)" | tee -a $LOG_FILE
-
-#check user has root preveligies are not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "you are running with root access" | tee -a $LOG_FILE
-fi
-
-#Validate function takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-then
-    echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
-else
-    echo -e "$2 is ... $R failure $N" | tee -a $LOG_FILE
-    exit 1
-fi
-}
+check_root
 
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "COPYING Mongodb repo" 
@@ -50,5 +22,4 @@ VALIDATE $? "Editing mongodb conf file for remote connection"
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "restarting mongodb"
 
-
-
+print_time
