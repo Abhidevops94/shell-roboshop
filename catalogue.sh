c3550b3f1715b81f6f3320706d529498c44d33a4
Abhi <abhidevops94@gmail.com>
2025-05-31 23:49:02 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -1,12 +1,79 @@
 #!/bin/bash
 
-source ./common.sh
-app_name=catalogue
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
-check_root
-app_setup
-nodejs_setup
-systemd_setup
+
+mkdir -p $LOGS_FOLDER
+echo "script started excuting at: $(date)" | tee -a $LOG_FILE
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+    echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
+else
+    echo -e "$2 is ... $R failure $N" | tee -a $LOG_FILE
+    exit 1
+fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling current NodeJS module"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling required NodeJS module"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Adding application User"
+else
+    echo -e "system user roboshop already created ...$Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading the catalogue app code to created app directory"
+
+rm -rf /app/*
+cd /app
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping catalogue"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue 
+VALIDATE $? "starting catalogue"
 
 cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
@@ -21,6 +88,7 @@ else
     echo -e "DATA is already loaded ... $Y SKIPPING $N"
 fi
 
-print_time
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-
+echo -e "script is completed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-source ./common.sh
-app_name=mongodb
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-check_root
+
+mkdir -p $LOGS_FOLDER
+echo "script started excuting at: $(date)" | tee -a $LOG_FILE
+
+#check user has root preveligies are not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+    echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
+else
+    echo -e "$2 is ... $R failure $N" | tee -a $LOG_FILE
+    exit 1
+fi
+}
 
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "COPYING Mongodb repo" 
@@ -22,4 +50,5 @@ VALIDATE $? "Editing mongodb conf file for remote connection"
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "restarting mongodb"
 
-print_time
+
+
